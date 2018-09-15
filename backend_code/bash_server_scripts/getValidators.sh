3f8ff8276c2865ce2bb61ac54e3c96fe4f3c0f8e
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-09-15 17:24:09 -0400

    Added full path to s3 file transfer

--- backend_code/bash_server_scripts/getValidators.sh
+++ backend_code/bash_server_scripts/getValidators.sh
@@ -1,4 +1,4 @@
 /home/ubuntu/goApps/bin/gaiacli stake validators --output json > /home/ubuntu/Cosmos-Validator-Utility/backend_code/bash_server_scripts/files/validators.json
 echo "Saved Validators to files/validators.json"
-/home/ubuntu/.local/bin/aws s3 cp files/validators.json s3://cosmos-validator-data/data/validators/validators.json
+/home/ubuntu/.local/bin/aws s3 cp /home/ubuntu/Cosmos-Validator-Utility/backend_code/bash_server_scripts/files/validators.json s3://cosmos-validator-data/data/validators/validators.json
 echo "Sent File to S3"
