4349e7e47015acc6ec4e3e34afa8caab43b940ae
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-09-24 20:46:02 -0400

    Added scripts from getActiveProposalsToS3

---
+++ backend_code/python_server_scripts/getActiveProposalsToS3/index.py
@@ -0,0 +1,39 @@
+import boto3
+import shlex, subprocess
+import time
+import json
+import datetime
+
+time_between_checks = 30
+
+#AWS Variables
+s3_resource = boto3.resource('s3')
+s3_bucket = 'cosmos-validator-data'
+s3_key_path = 'data/gov/active_proposals'
+
+def main():
+    #Fina all Voting Period proposals
+
+    #In a while loop because this should always be running
+    while True:
+        #Query Current Votes
+        current_proposals = subprocess.check_output(['/home/ubuntu/goApps/bin/gaiacli', 'gov', 'query-proposals', '--status', 'VotingPeriod']).split('\n')
+
+        #If None, sleep for time_between_no_props
+        if current_votes[0] == "No matching proposals found":
+            #Wait 30 Seconds
+            time.sleep(time_between_checks)
+            continue
+
+        #For all outstanding proposals, query them and send to s3
+        for next_proposal in current_votes:
+            proposal_id = current_votes[0].split('-')[0].strip()
+            current_proposal_results = subprocess.check_output(['/home/ubuntu/goApps/bin/gaiacli', 'gov', 'query-proposal', '--proposal-id', proposal_id])
+
+            key_full_path = s3_key_path + '/' + proposal_id + '.json'
+            obj = s3_resource.Object(s3_bucket, key_full_path)
+            obj.put(Body=current_proposal_results)
+
+        time.sleep(time_between_checks)
+
+main()
